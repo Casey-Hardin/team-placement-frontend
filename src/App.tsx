8b1db518e9171e665a7ed8213e4a340e7e5137ce
Casey-Hardin <casey_hardin@goodyear.com>
2024-03-30 13:05:07 -0400

    Initial commit This is the first commit.

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div id="page-content">
+        <h1>Team Placement</h1>
+        <p>Import data from Excel or JSON</p>
+        <input type="file" id="myFile" name="filename" />
+      </div>
+    </>
+  )
+}
+
+export default App
